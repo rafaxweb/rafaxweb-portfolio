6c4b03d8fec9dc7de327f7d26f6616d2bf5ee8ee
Rafael Salas <rafaxweb@gmail.com>
2023-02-08 20:16:15 +0100

    Added main files

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import MainPage from './pages/MainPage'
+
+function App() {
+  const [language, setLanguage] = useState("")
+  
+  useEffect(() => {
+    
+  })
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
