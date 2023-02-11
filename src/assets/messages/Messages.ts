3cd6ce891b0263b79d47076fe4e963e870bd5ba1
Rafael Salas <rafaxweb@gmail.com>
2023-02-11 16:26:22 +0100

    Added project files

---
+++ src/assets/messages/Messages.ts
@@ -0,0 +1,23 @@
+export class Messages {
+    
+    constructor() {
+    }
+
+    static Introduction = class {
+        public static Title = new Map();
+        public static Subtitle = new Map(); 
+        public static Description = new Map();
+
+        static {
+            this.Title.set("ES", "Hola, soy Rafael Salas");
+
+            this.Subtitle.set("ES", "Programador Full Stack");
+
+            this.Description.set("ES", "Programador Full Stack")
+        }
+    }
+
+    static HeaderMenu
+}
+
+
